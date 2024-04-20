511d8135b01fc70bdb98dbcb5bf7400a1bfd5c8b
EinJOJO <schlotmax507@gmail.com>
2024-04-21 00:21:54 +0200

    changes

--- src/main/java/it/einjojo/akani/essentials/util/EssentialMessage.java
+++ src/main/java/it/einjojo/akani/essentials/util/EssentialMessage.java
@@ -1,13 +1,15 @@
 package it.einjojo.akani.essentials.util;
 
 public enum EssentialMessage {
+    GENERIC_ERROR("generic.error"),
     TELEPORT_SUCCESS("teleport.success"),
+    TELEPORT_NOT_SELF("teleport.not_self"),
     TELEPORT_CANCEL("teleport.cancel"),
     TELEPORT_REQUEST("teleport.request");
     private final String key;
 
     EssentialMessage(String key) {
-        this.key = key;
+        this.key = "essentials." + key;
     }
 
     public String key() {
